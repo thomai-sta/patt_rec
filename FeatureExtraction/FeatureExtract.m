8124c71dc7d2fd583a4b15d07bea2f908e3025ef
thomai-sta <thomai.stathopoulou@gmail.com>
2015-10-04 21:49:23 +0200

    slf;kjg

--- FeatureExtraction/FeatureExtract.m
+++ FeatureExtraction/FeatureExtract.m
@@ -1,6 +1,6 @@
 function ret_s = FeatureExtract(s)
 
-ret_s = MoveCharacter(ScaleCharacter(RemovePenUp(Trim(s))));
-
+% ret_s = MoveCharacter(ScaleCharacter(RemovePenUp(Trim(s))));
+ret_s = MoveCharacter(ScaleCharacter(Trim(s)));
 end
 
